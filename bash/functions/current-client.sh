de9f6ec5028ef1299170666e1f0e9d00395d2842
Jonas Meinerz <jonas@thoughtbot.com>
2020-06-16 22:30:44 +0100

    Qonto: update prod scripts

--- bash/functions/current-client.sh
+++ bash/functions/current-client.sh
@@ -4,9 +4,15 @@ source ~/Qonto/devenv/activate
 eval "$(docker-machine env qonto)"
 
 function prod_api {
-  kubectl config use-context qonto-production
+  kubectl config use-context cbs-production
   cd $HOME/Qonto/qonto-helm-vars
   ./cli.sh --config environments/qonto/production.yaml run api-private-web --namespace=master --part-of=api -- bundle exec rails c
+}
+
+function prod_register {
+  kubectl config use-context cbs-production
+  cd $HOME/Qonto/qonto-helm-vars
+  ./cli.sh --config environments/qonto/production.yaml run register-private-web --namespace=master --part-of=register -- bundle exec rails c
 }
 
 function stg_pods {
