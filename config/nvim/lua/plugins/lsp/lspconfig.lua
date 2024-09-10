0bebf88a84e19a117a58b19c79f02c34054a9cf8
Jonas Calvi Meinerz <jonas@watershedclimate.com>
2024-09-10 15:26:00 +0000

    LSP config auto format

--- config/nvim/lua/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/neodev.nvim", opts = {} },
+    { "folke/neodev.nvim",                   opts = {} },
   },
   config = function()
     -- import lspconfig plugin
