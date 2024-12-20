2a28d54674641e72aba559e28697f5e86a53db04
Jonas Calvi Meinerz <jonas@watershedclimate.com>
2024-12-20 13:23:38 +0000

    Use prettierd

--- config/nvim/lua/plugins/lsp/mason.lua
+++ config/nvim/lua/plugins/lsp/mason.lua
@@ -40,7 +40,7 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier",   -- prettier formatter
+        "prettierd",  -- prettier formatter
         "stylua",     -- lua formatter
         "isort",      -- python formatter
         "black",      -- python formatter
