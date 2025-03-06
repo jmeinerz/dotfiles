96b8cc814025d44eed687bb5bbf0bfe13bb5ee10
Jonas Calvi Meinerz <jonas@watershedclimate.com>
2025-03-06 13:52:34 +0000

    nvim shortcuts for git hunk navigation

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,34 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', ']h', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']h', bang = true })
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[h', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[h', bang = true })
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+
+        map('n', '<leader>ga', gitsigns.stage_hunk)
+      end,
+    })
   end,
 }
