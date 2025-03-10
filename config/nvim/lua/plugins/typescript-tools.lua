0b53761757bb4380fe369340e7026cd7248c7063
Jonas Calvi Meinerz <jonas@watershedclimate.com>
2025-03-10 14:59:35 +0000

    NVIM: typescript-tools

---
+++ config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,12 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {},
+  config = function()
+    require("typescript-tools").setup({
+      settings = {
+        tsserver_max_memory = 13112,
+      }
+    })
+  end,
+}
